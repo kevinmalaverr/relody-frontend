a1fa5003392a5f886f8cf36a1b3b9d9a7ded32ae
unknown <kevinmalaverr@gmail.com>
2020-12-18 18:47:00 -0500

    [ADD] some changes

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Layout } from '../components/Layout'
 import { SearchBar } from '../components/SearchBar'
 
 const Songs = () => {
+  useEffect(() => {
+    window.fetch('http://localhost:3000/api/tutorials')
+      .then(res => res.json())
+      .then(result => {
+        console.log(result)
+      })
+  }, [])
+
   return (
     <Layout title='Buscar tutorailes de camciones'>
       <section className='hero'>
