33a057b059379de84c424d2acff55333f2e76e67
kevinmalaverr <kevinmalaverr@gmail.com>
2021-01-06 18:53:32 -0500

    [UPDATE] tutorialCard styles

--- src/components/TutorialCard.js
+++ src/components/TutorialCard.js
@@ -2,12 +2,6 @@ import React from 'react'
 import '../styles/components/TutorialCard.css'
 import { Link } from 'react-router-dom'
 
-const repeat = (n, action) => {
-  for (let index = 0; index < n; index++) {
-    action()
-  }
-}
-
 const listItems = (n, element) => {
   const list = []
   for (let index = 0; index < n; index++) {
@@ -16,31 +10,29 @@ const listItems = (n, element) => {
   return list
 }
 
-export const TutorialCard = ({ title = 'tutorial', difficult = 3, tutorialUrl = 'tutorial' }) => {
-  const difficultColor = (difficult < 2) ? 'easy' : (difficult === 3) ? 'medium' : 'hard'
+export const TutorialCard = ({ title = 'tutorial', difficult = Math.floor(Math.random() * (5 - 1)) + 1, tutorialUrl = 'tutorial' }) => {
+  const difficultColor = (difficult < 3) ? 'easy' : (difficult === 3) ? 'medium' : 'hard'
 
   return (
-    <Link to={tutorialUrl}>
-      <article className='tutorial-card'>
-        <img src='https://images-na.ssl-images-amazon.com/images/I/31PS7MIr-ZL._SX300_SY300_QL70_ML2_.jpg' alt='' className='tutorial-card__image' />
-        <div className='tutorial-card__a'>
-          <b>Nothing else matters</b>
-          <br />
-          <span>Metallica</span>
+    <article className='tutorial-card'>
+      <img src='https://images-na.ssl-images-amazon.com/images/I/31PS7MIr-ZL._SX300_SY300_QL70_ML2_.jpg' alt='' className='tutorial-card__image' />
+      <div className='tutorial-card__a'>
+        <b>Nothing else matters</b>
+        <br />
+        <span className='tutorial-card__artist'>Metallica</span>
+      </div>
+      <div className='tutorial-card__b'>
+        <div>
+          <i className='Icon_star_outline mr-1' /><span>15</span>
         </div>
-        <div className='tutorial-card__b'>
-          <div>
-            <i className='Icon_star_outline' /><span>15</span>
-          </div>
-          <div>
-            <span>difficult</span>
-            <div className='tutorial-card__difficult-list'>
-              {listItems(difficult, <div className={`tutorial-card__difficult-bar tutorial-card__difficult-bar--${difficultColor}`} />)}
-              {listItems(5 - difficult, <div className='tutorial-card__difficult-bar' />)}
-            </div>
+        <div className='tutorial-card__difficult'>
+          <span className='mr-1'>Difficult: </span>
+          <div className='tutorial-card__difficult-list'>
+            {listItems(difficult, <div className={`tutorial-card__difficult-bar tutorial-card__difficult-bar--${difficultColor}`} />)}
+            {listItems(5 - difficult, <div className='tutorial-card__difficult-bar' />)}
           </div>
         </div>
-      </article>
-    </Link>
+      </div>
+    </article>
   )
 }
--- src/components/TutorialList.js
+++ src/components/TutorialList.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../styles/components/TutorialList.css'
 import { connect } from 'react-redux'
 import { TutorialCard } from './TutorialCard'
 
 const TutorialList = () => {
   return (
-    <>
+    <section className='tutorial-list'>
       {[0, 1, 2, 3].map((card, i) => (
         <TutorialCard key={i} card />
       ))}
-    </>
+    </section>
   )
 }
 
